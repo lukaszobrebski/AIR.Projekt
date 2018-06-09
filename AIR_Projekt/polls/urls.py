88819ee972a8cdfcaf5405abea7cb244c5d3e8db
lukaszobrebski <lukasz.obrebski95@gmail.com>
2018-06-09 05:06:54 +0200

    Cosmetics

--- AIR_Projekt/polls/urls.py
+++ AIR_Projekt/polls/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.LoginUserAPI.as_view()),
     path('register', views.RegisterUserAPI.as_view()),
-    path('tasks', views.TasksAPI.as_view()),
+    path('tasks', views.TaskAPI.as_view()),
     path('history', views.HistoryAPI.as_view()),
-    path('notes', views.NotesAPI.as_view())
+    path('notes', views.NoteAPI.as_view())
 ]
