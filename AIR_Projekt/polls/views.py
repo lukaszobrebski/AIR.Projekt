941aa315471fa59ba43a565c023b1216c92001a0
lukaszobrebski <lukasz.obrebski95@gmail.com>
2018-06-09 05:05:29 +0200

    Completed the views for API

--- AIR_Projekt/polls/views.py
+++ AIR_Projekt/polls/views.py
@@ -11,41 +11,37 @@ from polls.models import *
 from polls.serializers import *
 
 """
-  path('', views.index, name='index'),
-    path('login', views.LoginUserAPI.as_view()),
-    path('register', views.RegisterUserAPI.as_view()),
-    path('tasks', views.TasksAPI.as_view()),
-    path('history', views.HistoryAPI.as_view()),
-    path('notes', views.NotesAPI.as_view())
+path('', views.index, name='index'),
+path('login', views.LoginUserAPI.as_view()),
+path('register', views.RegisterUserAPI.as_view()),
+path('tasks', views.TasksAPI.as_view()),
+path('history', views.HistoryAPI.as_view()),
+path('notes', views.NotesAPI.as_view())
 """
 class LoginUserAPI(APIView):
     def post(self, request): 	
-    	userNameRequest = request.POST.get('user_name')
-    	userPasswordRequest = request.POST.get('user_password')
+        userNameRequest = request.POST.get('user_name')
+        userPasswordRequest = request.POST.get('user_password')
+    	   
+        if User.objects.filter(user_name = userNameRequest, user_password = userPasswordRequest).exists():
+            return HttpResponse(status=200)
+        else:
+            return HttpResponseNotFound     
     	
-    	if User.objects.filter(user_name = userNameRequest, user_password = userPasswordRequest).exists():
-    		return HttpResponse(status=200)
-    	else:
-    		return HttpResponseNotFound
-    	
-    	
+      	
 class RegisterUserAPI(generics.CreateAPIView):
-    def post(self, request):
-    	model = User
-    	serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     
-class TasksAPI(APIView):
-    def post(self):
-        pass
-
-    def get(self):
-        pass
+class TasksAPI(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
     
 class HistoryAPI(APIView):
-    def get(self):
-    	pass
+    queryset = TaskHistory.objects.all()
+    serializer_class = TaskHistorySerializer
     
 class NotesAPI(APIView):
-    def post(self):
-    	pass
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
     
