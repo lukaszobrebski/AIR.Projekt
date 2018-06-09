6cd3a77a75c520295a4bba6f341cdc0e54f986c2
lukaszobrebski <lukasz.obrebski95@gmail.com>
2018-06-09 05:04:50 +0200

    Cosmetics

--- AIR_Projekt/polls/serializers.py
+++ AIR_Projekt/polls/serializers.py
@@ -4,33 +4,33 @@ from polls.models import *
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-      model = User
-      fields = ('user_name', 'user_password') #Excluded: user_id
-    	
+        model = User
+        fields = ('user_name', 'user_password') #Excluded: user_id
+    	       
     def create(self, validated_data):
-      return User.objects.create(**validated_data)
+        return User.objects.create(**validated_data)
     	
     def update(self, instance, validated_data):
-      instance.user_name = validated_data.get('user_name', instance.user_name)
-      instance.user_password = validated_data.get('user_password', instance.user_password)
-      instance.save()
-      return instance
+        instance.user_name = validated_data.get('user_name', instance.user_name)
+        instance.user_password = validated_data.get('user_password', instance.user_password)
+        instance.save()
+        return instance
     	
     	
 class NoteSerializer(serializers.ModelSerializer):
-   class Meta:
-      model = Note
-      fields = '__all__'		
+    class Meta:
+        model = Note
+        fields = '__all__'		
 		
 class TaskSerializer(serializers.ModelSerializer):
-   class Meta:
-      model = Task
-      fields = '__all__'
+    class Meta:
+        model = Task
+        fields = '__all__'
 			
 class TaskHistorySerializer(serializers.ModelSerializer):
-   class Meta:
-      model = TaskHistory
-      fields = '__all__'
+    class Meta:
+        model = TaskHistory
+        fields = '__all__'
 	
  
 
